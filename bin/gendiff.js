38f3615eaec668526da309d5950e14bee0f48a33
v.nam <v4lentine.nam@gmail.com>
2024-05-06 19:40:01 +0300

    1st step fixes

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,6 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .option('-v, --version', 'output the version number')
+  .option('-v, --version', 'output the version number');
 
 program.parse();
