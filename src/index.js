ebcfcdc55cc04ef549949bd398998f41896fb92c
v.nam <v4lentine.nam@gmail.com>
2024-05-06 20:54:41 +0300

    3rd step small fixes

--- src/index.js
+++ src/index.js
@@ -10,13 +10,33 @@ const gendiff = (path1, path2, opts) => {
     const fileContent1 = fs.readFileSync(file1Path, 'utf8');
     const fileContent2 = fs.readFileSync(file2Path, 'utf8');
     
+    const fileFormat1 = getFileFormat(file1Path);
+    const fileFormat2 = getFileFormat(file2Path);
+    
     console.log('Содержимое файла 1:');
+    console.log(`Формат файла 1: ${fileFormat1}`);
     console.log(fileContent1);
     
     console.log('Содержимое файла 2:');
+    console.log(`Формат файла 2: ${fileFormat2}`);
     console.log(fileContent2);
   } catch (err) {
     console.error('Произошла ошибка при чтении файла:', err);
+  }
+}
+
+const getFileFormat = (filePath) => {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case '.json':
+      return 'JSON';
+    case '.yml':
+      return 'YML';
+    case '.txt':
+      return 'TXT';
+    // Добавьте другие форматы файлов при необходимости
+    default:
+      return 'Неизвестный формат';
   }
 }
 
