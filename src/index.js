6390782cc3d53d5a7289832c6e2dd2c3a2d46078
v.nam <v4lentine.nam@gmail.com>
2024-05-11 19:28:34 +0300

    reroll last changes

--- src/index.js
+++ src/index.js
@@ -16,47 +16,18 @@ const parseFile = (filePath) => {
   }
 };
 
-// const genDiff = (filePath1, filePath2) => {
-//   const data1 = parseFile(filePath1);
-//   const data2 = parseFile(filePath2);
-
-//   const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
-  
-//   const diff = keys.map((key) => {
-//     if (_.has(data1, key) && _.has(data2, key)) {
-//       if (_.isEqual(data1[key], data2[key])) {
-//         return `  ${key}: ${data1[key]}`;
-//       }
-//       return `- ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
-//     } else if (_.has(data1, key)) {
-//       return `- ${key}: ${data1[key]}`;
-//     }
-//     return `+ ${key}: ${data2[key]}`;
-//   });
-
-//   return `{\n  ${diff.join('\n  ')}\n}`;
-// };
-
-const compareKeys = (data1, data2) => _.union(Object.keys(data1), Object.keys(data2)).sort();
-
-const generateDiff = (key, data1, data2) => {
-  if (_.isEqual(data1[key], data2[key])) {
-    return `  ${key}: ${data1[key]}`;
-  } else {
-    return [`- ${key}: ${data1[key]}`, `  + ${key}: ${data2[key]}`];
-  }
-};
-
 const genDiff = (filePath1, filePath2) => {
   const data1 = parseFile(filePath1);
   const data2 = parseFile(filePath2);
 
-  const keys = compareKeys(data1, data2);
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
   
   const diff = keys.map((key) => {
     if (_.has(data1, key) && _.has(data2, key)) {
-      const diffArray = generateDiff(key, data1, data2);
-      return Array.isArray(diffArray) ? diffArray.join('\n') : diffArray;
+      if (_.isEqual(data1[key], data2[key])) {
+        return `  ${key}: ${data1[key]}`;
+      }
+      return `- ${key}: ${data1[key]}\n  + ${key}: ${data2[key]}`;
     } else if (_.has(data1, key)) {
       return `- ${key}: ${data1[key]}`;
     }
