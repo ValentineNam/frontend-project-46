bb45978054364dca931e73a791f9f66eb74e772e
v.nam <v4lentine.nam@gmail.com>
2024-11-05 21:21:17 +0300

    one more try to fix

--- src/makediff.js
+++ src/makediff.js
@@ -5,16 +5,32 @@ const returnDiffObject = (data1, data2, keys) => {
   const diffObj = {};
   keys.forEach((key) => {
     if (!_.has(data1, key)) {
-      diffObj[key] = { type: 'added', value: data2[key] };
+      diffObj[key] = { 
+        type: 'added',
+        value: data2[key] 
+      };
     } else if (!_.has(data2, key)) {
-      diffObj[key] = { type: 'deleted', value: data1[key] };
+      diffObj[key] = { 
+        type: 'deleted',
+        value: data1[key] 
+      };
     } else if (_.isPlainObject(data1[key]) && _.isPlainObject(data2[key])) {
-      diffObj[key] = { type: 'nested', children: returnDiffObject(data1[key], data2[key], compareKeys(data1[key], data2[key])) };
+      diffObj[key] = { 
+        type: 'nested',
+        children: returnDiffObject(data1[key], data2[key], compareKeys(data1[key], data2[key]))
+      };
     } else if (_.has(data1, key) && _.has(data2, key)) {
       if (_.isEqual(data1[key], data2[key])) {
-        diffObj[key] = { type: 'unchanged', value: data1[key] };
+        diffObj[key] = { 
+          type: 'unchanged',
+          value: data1[key]
+        };
       } else {
-        diffObj[key] = { type: 'changed', oldValue: data1[key], newValue: data2[key] };
+        diffObj[key] = {
+          type: 'changed',
+          oldValue: data1[key],
+          newValue: data2[key]
+        };
       }
     }
   });
